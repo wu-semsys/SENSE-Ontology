d59e6433f1113392b2bd18af9581ed216cc7ab5f
Katrin Schreiberhuber <katrin.schreiberhuber@wu.ac.at>
2024-04-09 09:32:29 +0200

    commit Ontology V1

---
+++ README.md
@@ -0,0 +1,7 @@
+# SENSE Ontology
+
+- **SENSE v1.0.drawio**: Visual notation of the SENSE Ontology
+- **SENSE v1.0.drawio.xml**: XML Export of the drawio file to be uploaded to CHOWLK converter
+- **SENSE v1.0.ttl**: Turtle file of the SENSE Ontology as converted by the CHOWLK converter https://catalogue.fair-impact.eu/resources/chowlk
+- **SENSEComments.ttl**: Turtle file, which contains additional metadata information, which is not included in the CHOWLK.converted file. This metadata has to be updated manually, if new classes and properties are added to the drawio concept of the Ontology.
+- **ISSUES.md**: currently a local, manual list of issues to address in the future.
---
+++ extract_empty_object_definitions.py
@@ -0,0 +1,32 @@
+from rdflib import Graph, Namespace
+from rdflib.namespace import RDF, RDFS, OWL
+
+def extract_classes_and_object_properties(ttl_file):
+    # Load the Turtle file into an RDF graph
+    g = Graph()
+    g.parse(ttl_file, format='turtle')
+
+    # List to store owl:class instances
+    objects = []
+
+    # Iterate over each triple in the graph
+    for subj, pred, obj in g:
+        # Check if the triple represents a class
+        if pred == RDF.type:
+            objects.append(subj)
+
+    # Convert URIs to QNames using defined namespace prefixes
+    objects = [g.qname(uri) for uri in objects]
+
+    return sorted(objects)
+
+# Example usage
+if __name__ == "__main__":
+    ttl_file_root = "SENSE v1.0.ttl"  # Replace with the path to your Turtle file
+    objects = extract_classes_and_object_properties(ttl_file_root)
+
+    print("#empty objects:")
+    for uri in objects:
+        print(uri + " rdfs:comment \"\" .")
+
+    

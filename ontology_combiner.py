9cc8fe2e6619df2aa39cf6f9dfe8e0f761f2eb60
Gregor Käfer <gregor.kaefer@tuwien.ac.at>
2024-10-03 15:50:21 +0200

    Update ontology combiner and some prefixes in the ttl files

--- ontology_combiner.py
+++ ontology_combiner.py
@@ -1,25 +1,36 @@
 import rdflib
-import copy
+import os
 
-def combine_ontologies(ontology1_path, ontology2_path, output_path):
-    g1 = rdflib.Graph()
-    g1.parse(ontology1_path, format=rdflib.util.guess_format(ontology1_path))
+def combine_ontologies(ontology1_path, ontology2_paths, output_path):
+    combined_graph = rdflib.Graph()
 
-    g1_copy = copy.deepcopy(g1)
+    print(f"Parsing {ontology1_path}")
+    with open(ontology1_path, 'r', encoding='utf-8') as f:
+        data = f.read()
+    combined_graph.parse(data=data, format='turtle')
 
-    g2 = rdflib.Graph()
-    g2.parse(ontology2_path, format=rdflib.util.guess_format(ontology2_path))
-
-    g2_copy = copy.deepcopy(g2)
-
-    combined_graph = g1_copy + g2_copy
+    for ontology_path in ontology2_paths:
+        print(f"Parsing {ontology_path}")
+        with open(ontology_path, 'r', encoding='utf-8') as f:
+            data = f.read()
+        combined_graph.parse(data=data, format='turtle')
 
     combined_graph.serialize(destination=output_path, format='turtle')
     print(f"Combined ontology saved to {output_path}")
 
+base_dir = os.path.dirname(os.path.abspath(__file__))
+ontology_dir = os.path.join(base_dir, 'ontology files')
 
-ontology1_path = 'SENSE v1.0.ttl'
-ontology2_path = 'SENSEComments.ttl'
-output_path = 'sense.ttl'
+print(rdflib.__version__)
 
-combine_ontologies(ontology1_path, ontology2_path, output_path)
+ontology1_path = os.path.join(ontology_dir, 'SENSE v1.0.ttl')
+ontology2_paths = [
+    os.path.join(ontology_dir, 'SENSEComments.ttl'),
+    os.path.join(ontology_dir, 'SENSE v2.0_chowlk.ttl'),
+    os.path.join(ontology_dir, 'SENSE v2.0_inverses.ttl'),
+    os.path.join(ontology_dir, 'SENSE v2.0_oopsfixes.ttl')
+]
+
+output_path = os.path.join(base_dir, 'sense.ttl')
+
+combine_ontologies(ontology1_path, ontology2_paths, output_path)
